756ae15466c61e43e86d1a342c7cf164f2091436
MrFisror <bruno.souto1@estudante.ifgoiano.edu.br>
2026-04-20 20:45:35 -0300

    arrumar o nome que estava ao contrário

--- controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/request/HistoricoAcessoRequestDTO.java
+++ controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/request/HistoricoAcessoRequestDTO.java
@@ -1,11 +1,11 @@
 package br.com.gatekeeper.controle_acessos.dto.request;
 
-import br.com.gatekeeper.controle_acessos.model.enums.StatusHistoricoAcesso;
+import br.com.gatekeeper.controle_acessos.model.enums.HistoricoAcessoStatus;
 import lombok.Data;
 
 @Data
 public class HistoricoAcessoRequestDTO {
     private Integer usuarioId;
     private Integer moduloId;
-    private StatusHistoricoAcesso status;
+    private HistoricoAcessoStatus status;
 }
