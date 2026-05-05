1884609af4333d3fb910d7afd738688ae5d65fc2
MrFisror <bruno.souto1@estudante.ifgoiano.edu.br>
2026-05-05 01:03:55 -0300

    fix: impede criação de usuários com dados em branco

--- controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/request/UsuarioRequestDTO.java
+++ controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/request/UsuarioRequestDTO.java
@@ -1,12 +1,17 @@
 package br.com.gatekeeper.controle_acessos.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UsuarioRequestDTO {
 
+    @NotBlank(message = "O nome não pode estar em branco")
     private String nome;
     
+    @NotBlank(message = "O e-mail não pode estar em branco")
+    @Email(message = "O formato do e-mail é inválido")
     private String email;
     
     
@@ -22,4 +27,6 @@ public class UsuarioRequestDTO {
     
     private Integer perfilId;
     private String registroEmpregado;
+
+    
 }
