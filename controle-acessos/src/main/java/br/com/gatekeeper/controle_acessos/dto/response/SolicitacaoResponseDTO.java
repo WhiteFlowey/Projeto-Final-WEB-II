09dcbe997ba6aae106054139cd535a24450c9fc0
carla <carlax@outlook.com>
2026-06-15 14:38:25 -0300

    fix: arrumando

--- controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/response/SolicitacaoResponseDTO.java
+++ controle-acessos/src/main/java/br/com/gatekeeper/controle_acessos/dto/response/SolicitacaoResponseDTO.java
@@ -1,18 +1,20 @@
 package br.com.gatekeeper.controle_acessos.dto.response;
 
+import org.springframework.hateoas.RepresentationModel;
 import br.com.gatekeeper.controle_acessos.model.enums.SolicitacaoStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import java.time.LocalDateTime;
 
 @Data
-public class SolicitacaoResponseDTO {
+@EqualsAndHashCode(callSuper = false) // Necessário por causa da herança
+public class SolicitacaoResponseDTO extends RepresentationModel<SolicitacaoResponseDTO> {
 
     private Integer id;
     private String protocolo;
     private String justificativa;
     private SolicitacaoStatus status;
     private LocalDateTime dataSolicitacao;
-    
     private String nomeUsuario;
     private String nomeModulo;
 }
